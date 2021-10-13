9c66ae84930200daa99af1c97b214af187d2ff6d
20020299 <20020299@vnu.edu.vn>
2021-10-13 12:59:44 +0700

    EditAndDelete

--- TuDienCommandLine/src/DictionaryManagement.java
+++ TuDienCommandLine/src/DictionaryManagement.java
@@ -1,15 +1,17 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
-import java.io.*;
 
 public class DictionaryManagement {
     public static Word[] dic;
+    public static int wordNumber = 0;
     public static void insertFromFile() {
         try {
             Path path = Path.of("D:\\OOP\\TuDien.txt");
             List<String> dictionary_data = Files.readAllLines(path);
-            int num = dictionary_data.size();
-            dic = new Word[num];
+            wordNumber = dictionary_data.size();
+            dic = new Word[wordNumber];
             int pos = 0;
             String x, y;
             for (String word_data : dictionary_data) {
@@ -27,7 +29,7 @@ public class DictionaryManagement {
         Scanner sc = new Scanner(System.in);
         while(true) {
             String search = sc.next();
-            for (int i = 0; i < dic.length; i++) {
+            for (int i = 0; i < wordNumber; i++) {
                 if (dic[i].getWord_target().contains(search)) {
                     System.out.format("%-7s %-15s %-15s",
                             "",
@@ -44,21 +46,20 @@ public class DictionaryManagement {
 
         int num = Integer.parseInt(scan.nextLine());
         dic = new Word[num];
-        int pos = 0;
         while (num != 0) {
             String x, y;
-            System.out.println("nhap tu TA so "+(pos+1)+": ");
+            System.out.println("nhap tu TA so "+(wordNumber+1)+": ");
             x = scan.nextLine();
-            System.out.println("Nhap tu TV so "+(pos+1)+": ");
+            System.out.println("Nhap tu TV so "+(wordNumber+1)+": ");
             y = scan.nextLine();
-            dic[pos] = new Word(x, y);
+            dic[wordNumber] = new Word(x, y);
             num--;
-            pos++;
+            wordNumber++;
         }
     }
     public static void showAllWord() {
         System.out.println("No      |English        |Vietnamese");
-        for(int i = 0; i< dic.length; i++){
+        for(int i = 0; i< wordNumber; i++){
             System.out.format("%-7s %-15s %-15s",
                     i+1,
                     "|" + dic[i].getWord_target(),
@@ -75,6 +76,82 @@ public class DictionaryManagement {
             }
             fw.close();
         } catch (IOException e) {}
+    }
+    public static void deleteFromCommanLine() {
+        Scanner sc = new Scanner(System.in);
+        String delWord;
+        int m = 0;
+        if (wordNumber == 0) {
+            System.out.println("Dictionary is empty.");
+        } else {
+            System.out.println("Delete word: ");
+            delWord = sc.nextLine();
+            for (int i = 0; i < wordNumber; i++) {
+                if(dic[i].getWord_target().equals(delWord)) {
+                    m = 1;
+                    for (int j = i; j < wordNumber - 1; j++) {
+                        dic[j] = dic[j+1];
+                    }
+                }
+            }
+        }
+        if (m == 1) {
+            System.out.println("Deleted");
+        }
+        if (m == 0 && wordNumber > 0) {
+            System.out.println("Can't find delete word");
+        }
+        wordNumber--;
+    }
+    public static void editFromCommanLine() {
+        Scanner sc = new Scanner(System.in);
+        String edWord;
+        String edited = "";
+        int m;
+        if (wordNumber == 0) {
+            System.out.println("Dictionary is empty.");
+        } else {
+            int t = 0;
+            int k = 0;
+            int t1 = 0;
+            int t2 = 0;
+            System.out.println("Select the element you want to edit: ");
+            System.out.println("1. Word");
+            System.out.println("2. Mean");
+            System.out.println("Choice: ");
+            sc = new Scanner(System.in);
+            m = sc.nextInt();
+            sc = new Scanner(System.in);
+            System.out.println("Edit word: ");
+            edWord = sc.nextLine();
+            for (int i = 0; i < wordNumber; i++) {
+                if (dic[i].getWord_target().equals(edWord)) {
+                    t = 1;
+                    if (m == 1) {
+                        t1 = 1;
+                        k = i;
+                    }
+                    if (m == 2) {
+                        t2 = 1;
+                        k = i;
+                    }
+                }
+            }
+            if (t == 1 && wordNumber > 0) {
+                sc = new Scanner(System.in);
+                System.out.println("Edited to: ");
+                edited = sc.nextLine();
+            }
+            if (t1 == 1) {
+                dic[k].setWord_target(edited);
+            }
+            if (t2 == 1) {
+                dic[k].setWord_explain(edited);
+            }
+            if (t == 0 && wordNumber > 0) {
+                System.out.println("Can't find edit word");
+            }
+        }
     }
 
 }
